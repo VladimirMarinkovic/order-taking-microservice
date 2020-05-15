924c083262eb8860542605002c54958e2e6336ea
Vladimir <exxperiment899@gmail.com>
2020-05-15 11:32:46 +0200

    add microservices

---
+++ zuul-gateway/src/test/java/vlada/spring/zuulgateway/ZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package vlada.spring.zuulgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
